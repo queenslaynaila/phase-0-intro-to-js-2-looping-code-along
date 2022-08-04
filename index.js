f3fc8e9e39868e0a35d32f5a9377b61d85dba679
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-04 22:50:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names=["Guadalupe", "Ollie", "Aki"], events = "surprose") {
+    let returnArray = [];
+    for(let i = 0; i < names.length; i++) {
+        returnArray[i] = `Thank you, ${names[i]}, for the wonderful ${events} gift!`
+    }
+    return returnArray;
+}
+console.log(writeCards())
+
+function countDown(int) {
+    while (int>= 0) {
+        console.log(int)
+        int--
+    }
+}
+countDown(10)
